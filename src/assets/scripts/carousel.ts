9a31f532e2f56c5a8059668caa146c644ae27f93
Erik Lorenz <erikslorenz@gmail.com>
2019-04-18 15:17:53 -0700

    Finished carousel completely now

--- src/assets/scripts/carousel.ts
+++ src/assets/scripts/carousel.ts
@@ -8,6 +8,41 @@ function carousel() {
   // Figure out scroll amount with 3 sections
   const scrollAmount = Math.floor(carousel.scrollWidth / 3);
 
+  // Activate the circles
+  function activateCircle(sectionNumber: number) {
+    const circle = document.querySelector(
+      '#circle' + sectionNumber,
+    ) as HTMLButtonElement;
+    const section = document.querySelector(
+      '#section' + sectionNumber,
+    ) as HTMLElement;
+
+    const config = {
+      threshold: 0.5,
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          // Add active class
+          circle.className = 'circle active';
+          console.log('Entering');
+        }
+
+        if (!entry.isIntersecting) {
+          circle.className = 'circle';
+          console.log('Leaving');
+        }
+      });
+    }, config);
+
+    observer.observe(section);
+  }
+
+  activateCircle(1);
+  activateCircle(2);
+  activateCircle(3);
+
   // Left arrow scrolling
   leftArrow.addEventListener('click', () => {
     const currentPosition = carousel.scrollLeft;
