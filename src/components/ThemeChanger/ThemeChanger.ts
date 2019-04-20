172df8db019464dc76a615f0b4fb6add5eb95acb
erlorenz <38394390+erlorenz@users.noreply.github.com>
2019-04-20 10:57:14 -0700

    Got tooltip for theme changer working okay

--- src/components/ThemeChanger/ThemeChanger.ts
+++ src/components/ThemeChanger/ThemeChanger.ts
@@ -1,7 +1,11 @@
-import { LitElement, html, customElement, css } from 'lit-element';
+import { LitElement, html, customElement, css, property } from 'lit-element';
 
 @customElement('theme-changer')
 class ThemeChanger extends LitElement {
+
+  @property()
+  private tooltipColor = '';
+
   static get styles() {
     return css`
       :host {
@@ -56,6 +60,41 @@ class ThemeChanger extends LitElement {
         background-color: #eb2188;
       }
 
+      label {
+        position: absolute;
+        font-size: 1.2rem;
+        height: 30px;
+        width: 100%;
+        color: white;
+        background-color: var(--secondary-theme-color);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        bottom: -40px;
+        left: 50%;
+        transform: translateX(-50%);
+        border-radius: 10px;
+      }
+
+      .arrow {
+        display: block;
+        position: absolute;
+        left: 50%;
+        transform: translateX(-50%);
+        top: -20px;
+        height: 0;
+        width: 0;
+        border: 10px solid var(--secondary-theme-color);;
+        border-left-color: transparent;
+        border-top-color: transparent;
+        border-right-color: transparent;
+      }
+
+      .arrow.left {
+        transform: translateX(-50%);
+        left: 15%;
+      }
+
       @media (max-width: 450px) {
         :host {
           display: none;
@@ -70,30 +109,35 @@ class ThemeChanger extends LitElement {
         <div
           class="circle circle--red"
           @click="${() => this.changeTheme('red')}"
+          @mouseover="${() => this.showTooltip('red')}"
+          @mouseout="${() => this.hideTooltip('red')}"
         ></div>
       </div>
       <div class="swatch">
         <div
           class="circle circle--green"
           @click="${() => this.changeTheme('green')}"
+          @mouseover="${() => this.showTooltip('green')}"
+          @mouseout="${() => this.hideTooltip('green')}"
         ></div>
       </div>
       <div class="swatch">
         <div
           class="circle circle--pink"
           @click="${() => this.changeTheme('pink')}"
+          @mouseover="${() => this.showTooltip('pink')}"
+          @mouseout="${() => this.hideTooltip('pink')}"
         ></div>
       </div>
+      ${this.tooltipColor ? html`
+      <label>
+      ${this.tooltipColor}
+      </label>
+      ` : null}
     `;
   }
 
-  changeTheme(color: string) {
-    const body = document.querySelector('body') as HTMLElement;
-    body.className = '';
-    body.classList.add(color + '-theme');
-    console.log('Changed theme to', color);
-    localStorage.setItem('theme', color);
-  }
+
 
   firstUpdated() {
     // Check Local storage otherwise use default theme
@@ -108,6 +152,28 @@ class ThemeChanger extends LitElement {
     setTimeout(() => {
       if (!this.classList.contains('show')) this.classList.add('show');
     }, 5000);
+  }
+
+  changeTheme(color: string) {
+    const body = document.querySelector('body') as HTMLElement;
+    body.className = '';
+    body.classList.add(color + '-theme');
+    console.log('Changed theme to', color);
+    localStorage.setItem('theme', color);
+  }
+
+  showTooltip(color: string) {
+    let theme = '';
+    if (color === 'red') theme = 'black/red'; 
+    if (color === 'green') theme = 'blue/green';
+    if (color === 'pink') theme = 'blue/pink';
+
+    this.tooltipColor = theme;
+    console.log(theme);
+  }
+
+  hideTooltip(color: string) {
+      this.tooltipColor = '';
   }
 }
 
