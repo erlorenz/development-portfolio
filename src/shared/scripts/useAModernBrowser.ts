c4fd6eb8704e00c61c92261ab7d161e20c6f4964
Erik Lorenz <erikslorenz@gmail.com>
2019-04-22 20:42:15 -0700

    Finished!

--- src/shared/scripts/useAModernBrowser.ts
+++ src/shared/scripts/useAModernBrowser.ts
@@ -1,7 +1,13 @@
 function useAModernBrowser() {
-  alert(
-    'This is a portfolio site showcasing new features and APIs. Use a modern browser!',
-  );
+  if (!window.customElements) {
+    alert(
+      'This is a portfolio site showcasing new features and APIs. Use a modern browser!',
+    );
+
+    console.log(
+      '[FROM ERIK] Please use a better browser. I am intentionally using bleeding edge APIs and features.',
+    );
+  }
 }
 
 export default useAModernBrowser;
